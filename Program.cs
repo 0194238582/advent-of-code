8ec11c160501b9b3b7f5c85bf75ea88de18b966a
0194238582 <gw10fezouimohamed@glow.sch.uk>
2022-12-05 21:52:37 +0000

    day5

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿namespace advent_of_code
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            // Day 1
+            //new day1().Run();
+
+            // Day 2
+            //new day2().Run();
+
+            // Day 3
+            //new day3().Run();
+            
+            // Day 4
+            //new day4().Run();
+
+            // Day 5
+            new day5().Run();
+        }
+    }
+}
